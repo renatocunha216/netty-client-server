135258f042c0eab6990c482a3acc76a332c9a62c
rbcunha79 <rbcunha@yahoo.com>
2023-08-08 14:03:49 -0300

    -

--- netty-common/src/main/java/br/com/rbcti/common/messages/FileTransferDataMessage.java
+++ netty-common/src/main/java/br/com/rbcti/common/messages/FileTransferDataMessage.java
@@ -1,16 +1,22 @@
 package br.com.rbcti.common.messages;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+import br.com.rbcti.common.util.ByteBufferWorker;
+
 /**
  *
  * Message structure:<br>
  *
- * [len][id][version][code][uuid]<br>
- * [len]            2 bytes - uint<br>
- * [id]             2 bytes - uint<br>
- * [version]        1 bytes - uint<br>
- * [usn]            8 bytes - ulong (unique sequential number)<br>
- * [?]              4 bytes - uint<br>
- * [?]              n bytes - string ascii
+ * [len][id][version][usn][fileNameLength][fileName][fileData]<br>
+ * [len]             2 bytes - uint<br>
+ * [id]              2 bytes - uint<br>
+ * [version]         1 bytes - uint<br>
+ * [usn]             8 bytes - ulong (unique sequential number)<br>
+ * [fileNameLength]  1 bytes - uint<br>
+ * [fileName]        n bytes - uint<br>
+ * [fileData]        n bytes - string ascii
  *
  * @author Renato Cunha
  * @see StartFileTransferMessage
@@ -22,9 +28,80 @@ public class FileTransferDataMessage implements SimpleMessage {
     private static final int ID = Messages.FILE_TRANSFER_DATA;
     private static final short VERSION = 0x01;
 
+    private static final int MAX_MESSAGE_LENGTH = 65535;
+
+    private static final int HEADER_LENGTH = 13;
+    private static final int LENGTH_FIELD_FILE_LENGTH = 1;
+
+    private long usn;
+    private String fileName;
+    private byte[] fileData;
+    private byte[] data;
+
+    public FileTransferDataMessage(long _usn, String _fileName,  byte[] _data) {
+
+        byte[] bytesFileName = _fileName.getBytes(Charset.forName("US-ASCII"));
+
+        if (bytesFileName.length > 255) {
+            throw new IllegalArgumentException("max file name length is 255");
+        }
+
+        if (_data.length > (MAX_MESSAGE_LENGTH - HEADER_LENGTH - LENGTH_FIELD_FILE_LENGTH + 2)) {
+            throw new IllegalArgumentException("maximum data size exceeded");
+        }
+
+        int dataLen = HEADER_LENGTH + LENGTH_FIELD_FILE_LENGTH + bytesFileName.length + _data.length;
+
+        ByteBuffer buffer = ByteBuffer.allocate(dataLen);
+
+        buffer.putShort((short) (dataLen - 2));
+        buffer.putShort((short) ID);
+        buffer.put((byte) VERSION);
+        buffer.putLong(_usn);
+
+        ByteBufferWorker.putUnsignedByte(buffer, bytesFileName.length);
+        buffer.put(bytesFileName);
+        buffer.put(_data);
+
+        this.data = buffer.array();
+
+        this.usn = _usn;
+        this.fileName = _fileName;
+        this.fileData = _data;
+    }
+
+    public FileTransferDataMessage(byte[] _data) {
+
+        ByteBuffer buffer = ByteBuffer.wrap(_data);
+
+        int _len = ByteBufferWorker.getUnsignedShort(buffer);
+        int _id = ByteBufferWorker.getUnsignedShort(buffer);
+        short _version = ByteBufferWorker.getUnsignedByte(buffer);
+
+        if ((_len != (_data.length - 2)) || (_id != ID) || (_version != VERSION)) {
+            throw new IllegalArgumentException("invalid fields.");
+        }
+
+        this.usn = buffer.getLong();
+
+        int fileNameLength = ByteBufferWorker.getUnsignedByte(buffer);
+        byte[] fileNameBytes = new byte[fileNameLength];
+        buffer.get(fileNameBytes);
+        byte[] dst = new byte[_data.length - (HEADER_LENGTH + LENGTH_FIELD_FILE_LENGTH + fileNameLength)];
+        buffer.get(dst);
+
+        this.fileName = new String(fileNameBytes, Charset.forName("US-ASCII"));
+        this.fileData = dst;
+
+        this.data = new byte[_data.length];
+        System.arraycopy(_data, 0, this.data, 0, _data.length);
+
+    }
+
+
     @Override
     public int getLength() {
-        return 0;
+        return this.data.length;
     }
 
     @Override
@@ -39,7 +116,19 @@ public class FileTransferDataMessage implements SimpleMessage {
 
     @Override
     public byte[] getData() {
-        return null;
+        return this.data;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    public long getUsn() {
+        return usn;
+    }
+
+    public String getFileName() {
+        return fileName;
     }
 
 }
--- netty-common/src/test/java/br/com/rbcti/common/messages/FileTransferDataMessageTest.java
+++ netty-common/src/test/java/br/com/rbcti/common/messages/FileTransferDataMessageTest.java
@@ -1,10 +1,112 @@
 package br.com.rbcti.common.messages;
 
+import static org.testng.Assert.assertEquals;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+import org.testng.annotations.Test;
+
 /**
-*
-* @author Renato Cunha
-*
-*/
+ *
+ * @author Renato Cunha
+ *
+ */
 public class FileTransferDataMessageTest {
+
+    @Test
+    public void testCorrectLength() {
+
+        System.out.println(getClass().getSimpleName() + ".testCorrectLength");
+
+        final int FIELDS_LENGTH = 14;
+        final int DATA_LENGTH = 16384;
+
+        long usn = 57949191L;
+        String fileName = "events-2023-08-07-112207.dat";
+
+        FileTransferDataMessage fileTransferDataMessage = new FileTransferDataMessage(usn, fileName, new byte[DATA_LENGTH]);
+
+        if (fileTransferDataMessage.getVersion() == (short) 1) {
+            assertEquals(fileTransferDataMessage.getLength(), FIELDS_LENGTH + fileName.length() + DATA_LENGTH);
+        }
+    }
+
+
+    @Test
+    public void testCorrectRandomContent() {
+
+        System.out.println(getClass().getSimpleName() + ".testCorrectRandomContent");
+
+        long usn = (long) (Math.random() * 1000000000000000L);
+        int fileLength = (int) (Math.random() * 10000);
+
+        byte[] fileData = new byte[fileLength];
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(fileData);
+
+        String fileName = "events-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HHmmss")) + ".dat";
+
+        FileTransferDataMessage fileTransferDataMessage1 = new FileTransferDataMessage(usn, fileName, fileData);
+
+        byte[] fileData1 = fileTransferDataMessage1.getData();
+
+        FileTransferDataMessage fileTransferDataMessage2 = new FileTransferDataMessage(fileData1);
+
+        assertEquals(Arrays.equals(fileTransferDataMessage2.getData(), fileData1), true);
+
+        assertEquals(fileTransferDataMessage1.getId(), fileTransferDataMessage2.getId());
+        assertEquals(fileTransferDataMessage1.getVersion(), fileTransferDataMessage2.getVersion());
+        assertEquals(fileTransferDataMessage1.getUsn(), fileTransferDataMessage2.getUsn());
+        assertEquals(fileTransferDataMessage1.getFileName(), fileTransferDataMessage2.getFileName());
+        assertEquals(Arrays.equals(fileTransferDataMessage1.getFileData(), fileTransferDataMessage2.getFileData()), true);
+    }
+
+    @Test
+    public void testCorrectMaxContent() {
+
+        System.out.println(getClass().getSimpleName() + ".testCorrectMaxContent");
+
+        final long USN = Long.MAX_VALUE;
+        final int MAX_MESSAGE_LENGTH = 65535;
+        final int MESSAGE_FIELDS_LENGTH = 12;
+
+        // Build file name
+        long fileId = (int) (Math.random() * 100000000000000000L);
+
+        String bigFileName = "events-" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HHmmss")) + "_";
+        int missing = 255 - 4 - bigFileName.length();
+        bigFileName += StringUtils.leftPad(Long.valueOf(fileId).toString(), missing, "0") + ".dat";
+
+        // Build data file
+        int fileLength = MAX_MESSAGE_LENGTH - MESSAGE_FIELDS_LENGTH - bigFileName.length();
+
+        byte[] fileData = new byte[fileLength];
+
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(fileData);
+
+        FileTransferDataMessage fileTransferDataMessage1 = new FileTransferDataMessage(USN, bigFileName, fileData);
+
+        byte[] fileData1 = fileTransferDataMessage1.getData();
+
+        FileTransferDataMessage fileTransferDataMessage2 = new FileTransferDataMessage(fileData1);
+
+        assertEquals(Arrays.equals(fileTransferDataMessage2.getData(), fileData1), true);
+
+        assertEquals(fileTransferDataMessage1.getId(), fileTransferDataMessage2.getId());
+        assertEquals(fileTransferDataMessage1.getVersion(), fileTransferDataMessage2.getVersion());
+        assertEquals(fileTransferDataMessage1.getUsn(), fileTransferDataMessage2.getUsn());
+        assertEquals(fileTransferDataMessage1.getFileName(), fileTransferDataMessage2.getFileName());
+        assertEquals(Arrays.equals(fileTransferDataMessage1.getFileData(), fileTransferDataMessage2.getFileData()), true);
+
+        assertEquals(fileTransferDataMessage2.getFileName(), bigFileName);
+        assertEquals(fileTransferDataMessage2.getUsn(), USN);
+        assertEquals(Arrays.equals(fileTransferDataMessage2.getFileData(), fileData), true);
+    }
 
 }
